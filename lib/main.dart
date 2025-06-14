e9ef360e5126a842b9e06a2b2535dbe314c348ac
Rawnak003 <shmrawnak@gmail.com>
2025-06-14 12:00:07 +0600

    Add firebase and crashlytics setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
+import 'dart:ui';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:piechat/src/core/app/app.dart';
+import 'package:piechat/src/core/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   runApp(PieChat());
 }
 
